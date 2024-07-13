576329ed81ac7228dd8fec1968de097254521d99
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-07-13 13:19:19 -0700

    Added piechart with mock data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
+import Chart from "chart.js/auto"
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import PieChart from "./compnents/PieChart";
+import { Data } from "./utils/Data"
 import './App.css';
 
+Chart.register(CategoryScale);
+
 function App() {
+  const [chartData, setChartData] = useState({
+    labels: ['Right-handed', 'Left-handed'],
+    datasets: [
+      {
+        label: "Handedness Distribution",
+        data: [Data.rightHanded, Data.leftHanded],
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  });
+
   return (
     <div className="App">
       <h1>Are you a lefty or a righty ?</h1>
-      <div><p>(Pie Chart goes here)</p></div>
+      <div>
+        <PieChart chartData={chartData} />
+      </div>
       <div>
         <button>Left</button>
         <button>Right</button>
---
+++ src/utils/Data.js
@@ -0,0 +1,5 @@
+// utils/Data.js
+export const Data = {
+    leftHanded: 50,
+    rightHanded:100
+};
