16d835755dedaf134dafdbc6e06e56eb841342b7
laoloser <slaux86@gmail.com>
2024-07-15 20:55:14 -0700

    Adding Styling

--- src/compnents/PieChart.js
+++ src/compnents/PieChart.js
@@ -3,7 +3,7 @@ import { Pie } from "react-chartjs-2";
 
 function PieChart({ chartData }) {
   return (
-    <div className="chart-container" style={{ width: '50%', margin: 'auto' }}>
+    <div className="chart-container" style={{ width: '50%', margin: 'auto', maxWidth: '400px', minWidth: '250px' }}>
       <h2 style={{ textAlign: "center" }}>Pie Chart</h2>
       <Pie
         data={chartData}
